aa77e1d63c51ef64036b313ee3928669943de769
djdinnebeil <djdinnebeil@gmail.com>
2025-03-13 15:00:08 -0400

    Added function to flip a coin

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -3,5 +3,9 @@ import random
 def heads_or_tails():
     return ['heads', 'tails'][random.randint(0,1)]
 
+def flip_coin(flips=1):
+    for _ in range(flips):
+        print(heads_or_tails())
+
 if __name__ == '__main__':
-    print(heads_or_tails())
+    flip_coin(10)
